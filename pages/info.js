f10603733384b3215e48f2f8feb3f33bad325707
Flexolk <dev@flexolk.me>
2025-01-02 23:20:20 +0800

    feat: add info

--- pages/info.js
+++ pages/info.js
@@ -21,7 +21,6 @@ export default function Info({ allPostsData }) {
                 <h2>普洱自介</h2>
                 <p>
                     嗨你好！我是普洱！(也可以叫我 Flex！)目前是研究生，專注於資安相關、網路、網頁等資訊領域。
-
                 </p>
                 <ul>
                     <li>興趣(我覺得我要多點興趣)：打遊戲、出毛、畫畫、寫程式</li>
@@ -35,9 +34,21 @@ export default function Info({ allPostsData }) {
                     <li><Link href="https://x.com/SmoothieNoIce">X/Twitter(毛毛居多)</Link></li>
                     <li><Link href="https://www.plurk.com/flexolk">Plurk</Link></li>
                     <li><Link href="https://github.com/SmoothieNoIce">Github(程式)</Link></li>
+                    <li><Link href="https://steamcommunity.com/id/SmoothieNoIce/">Steam(遊戲)</Link></li>
                 </ul>
 
-                <h3>毛毛</h3>
+                <h3>主推毛毛們</h3>
+                <ul>
+                    <li><Link href="https://x.com/Kechisy">Sanu</Link></li>
+                    <li><Link href="https://x.com/Hydreigon_owo">Kushamo</Link></li>
+                    <li><Link href="https://x.com/josh911031">Arnold 阿諾</Link></li>
+                    <li><Link href="https://x.com/Rikujyun">凜紅</Link></li>
+                    <li><Link href="https://x.com/loynuwu">Loyn</Link></li>
+                    <li><Link href="https://x.com/wade990047">龍沁</Link></li>
+                    <li><Link href="https://x.com/AirouCat620">Airou</Link></li>
+                </ul>
+
+                <h3>自己的毛毛</h3>
                 <p>目前有一套毛！裝師是<Link href="https://t.co/D5piYVgy1P">木有(獸喜鍋工作室)</Link></p>
                 <img src="/images/photo/photo_2025-01-02_13-07-50.jpg" alt="Picture of the author" />
                 <p className={utilStyles.imgText1}>りしれ供さ小</p>
