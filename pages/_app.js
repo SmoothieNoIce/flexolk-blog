da85c89b648c41f8f6d6c455ba63eb6e2f3fa30c
Flexolk <dev@flexolk.me>
2025-01-04 10:54:22 +0800

    feat: update 1/4

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/global.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId="G-PGZRZ606MH" />
+    </>
+  )
 }
